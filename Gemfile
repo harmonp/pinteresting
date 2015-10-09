604b79a30677aa5239b4e995b5d73f10e1051b42
harmonp <harmon.a.phillips@gmail.com>
2015-10-09 17:36:23 -0400

    Added Devise Gem

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+ruby '2.1.2'
 
-
+#devise for adding Users
+gem 'devise', '3.5.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use SCSS for stylesheets
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #Required for Heroku/Devise
+  #Set this to actual host
+  config.action_mailer.default_url_options = { host: 'omr-pinteresting-clone.herokuapp.com'}
+
 end
